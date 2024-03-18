982e6b5175bffafed40d4683f311fe3294825edb
SamiNayeem <saminayeem8@gmail.com>
2024-03-18 13:27:12 +0600

    vendor login frontend done

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import GetStarted from './src/Screens/GetStarted/GetStarted';
 import UserLogin from './src/Screens/UserLogin/UserLogin';
+import VendorLogin from './src/Screens/VendorLogin/VendorLogin';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <GetStarted /> */}
-      <UserLogin />
+      {/* <UserLogin /> */}
+      <VendorLogin />
       <StatusBar style="auto" />
     </View>
   );
